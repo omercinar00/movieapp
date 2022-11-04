4679b2dac3d9598e63505ed64bae579ee9b446b3
ÖmerÇınar <omercinarr00@gmail.com>
2022-11-04 23:25:13 +0300

    al al

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,16 +2,13 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div
-      className="d-flex justify-content-center "
-      
-    >
+    <div className="d-flex justify-content-center">
       <div className="form-image d-none d-md-block ">
         <img src={"https://picsum.photos/800/800"} alt="sample-movie" />
       </div>
       <div className="register-form">
         <h1 className="form-title display-3 ">Login</h1>
-        <form id="register">
+        <form id="register" >
           <div className="mb-3">
             <label htmlFor="email" className="form-label">
               Email
@@ -22,6 +19,7 @@ const Login = () => {
               id="email"
               placeholder="Enter your email adress.."
               required
+              
             />
           </div>
           <div className="mb-3">
@@ -34,16 +32,22 @@ const Login = () => {
               id="password"
               placeholder="Enter your password.."
               required
+              
             />
           </div>
-          <div className="link">Forgot Password?</div>
+          <div className="link" >
+            Forgot Password?
+          </div>
           <input
             type="submit"
             className="btn btn-primary form-control"
             value="Login"
           />
         </form>
-        <button className="btn btn-primary form-control">
+        <button
+          className="btn btn-primary form-control"
+          
+        >
           Continue with Google
         </button>
       </div>
