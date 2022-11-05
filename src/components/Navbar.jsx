8873a885d6eae63e3577b830ba0b259be2a65dcc
ÖmerÇınar <omercinarr00@gmail.com>
2022-11-05 21:28:44 +0300

    fixed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { logOut } from '../auth/Firebase';
+import { AuthContext } from '../context/AuthContext';
+
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const {currentUser} = useContext(AuthContext)  
   return (
     <div>
       <nav className="navbar navbar-expand-lg ">
@@ -11,12 +15,14 @@ const Navbar = () => {
             <h4>React Movie App</h4>
           </Link>
           <div className="d-flex text-white align-items-center ">
-            {false ? (
+            { currentUser ? (
               <>
-                <h5 className="mb-0 text-capitalize"></h5>
+                <h5 className="mb-0 text-capitalize">
+                  {currentUser?.displayName}
+                </h5>
                 <button
                   className="ms-2 btn btn-outline-light"
-                  onClick={()=>navigate("/")}
+                  onClick={()=>logOut()}
                 >
                   Logout
                 </button>
