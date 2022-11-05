7a9571e699fddacfe263ba1739e9c35e086182b6
ÖmerÇınar <omercinarr00@gmail.com>
2022-11-06 01:27:43 +0300

    moviedetails add

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 const MovieDetails = () => {
 
@@ -12,6 +12,8 @@ const MovieDetails = () => {
   const API_KEY = "d30a8fd3c99b3b48b8263203351de7ab";
   const DetailsUrl = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`;
   const videoUrl = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}`;
+  const ImageUrl = "https://image.tmdb.org/t/p/w1280";
+
 
   const {
     title,
@@ -51,14 +53,48 @@ const MovieDetails = () => {
   
 
   return (
-    <div className="card w-75 m-auto my-3">
-      <div className="card-body">
-        <div className="ratio ratio-16x9">
-          <iframe
-            src={`https://www.youtube.com/embed/${video}?autoplay=1&mute=1`}
-            title="YouTube video"
-            allowFullScreen
-          ></iframe>
+    <div className="container py-5">
+      <h1 className="text-center">{title}</h1>
+      <div className="card w-75 m-auto my-3">
+        <div className="card-body">
+          <div className="ratio ratio-16x9">
+            <iframe
+              src={`https://www.youtube.com/embed/${video}?autoplay=1&mute=1`}
+              title="YouTube video"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
+      </div>
+      <div className="card mb-3">
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img
+              src={ImageUrl + poster_path}
+              className="img-fluid rounded-start"
+              alt="..."
+            />
+          </div>
+          <div className="col-md-8 d-flex flex-column ">
+            <div className="card-body">
+              <h5 className="card-title">Overview</h5>
+              <p className="card-text">{overview}</p>
+            </div>
+            <ul className="list-group ">
+              <li className="list-group-item">
+                {"Release Date : " + release_date}
+              </li>
+              <li className="list-group-item">{"Rate : " + vote_average}</li>
+              <li className="list-group-item">
+                {"Total Vote : " + vote_count}
+              </li>
+              <li className="list-group-item">
+                <Link to={-1} className="card-link">
+                  Go Back
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
@@ -66,3 +102,4 @@ const MovieDetails = () => {
 }
 
 export default MovieDetails
+
